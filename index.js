4d1c9f53b044cb8b8adb3a11f32416c0f8a5c4cb
Veselin Stoyanov <veselin@stan.vision>
2020-02-03 11:05:48 +0200

    HTML generation

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const generateFile = require('./lib/generateFile');
 
 const options = {
+  jsEntry: 'bundle',
+  processHTML: 'yes',
+  htmlPreference: 'handlebars',
   processStyles: 'yes',
-  stylesPreference: 'css-in-js',
-  stylesEntry: 'style',
-  stylesType: 'css',
+  stylesPreference: 'separate-files',
+  stylesType: 'scss',
+  stylesEntry: 'load',
+  processImages: 'no',
+  resolveSize: 0,
+  imageUrlResolve: false,
+  imageOptimization: false,
+  svgOptimization: false,
+  optimizations: 'no',
+  devServer: 'no',
+  splitChunks: false,
+  webp: false,
+  criticalCss: false,
+  purgeCss: false,
 };
 
 generateFile(options);
