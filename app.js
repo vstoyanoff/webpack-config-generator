3146f5c483a9c7beba92f13487f673c76c2cb3e8
Veselin Stoyanov <veselin@stan.vision>
2020-03-01 20:35:52 +0200

    Recaptcha verification

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const rimraf = require('rimraf');
 const generateFiles = require('./lib/generateFiles');
+const verifyRecaptcha = require('./lib/verifyRecaptcha.js');
 
 const app = express().use(
   '*',
@@ -24,6 +25,24 @@ app.get('/remove', (req, res) => {
 });
 
 app.post('/generate', bodyParser.json(), async (req, res, next) => {
+  if (
+    req.body.gResp === '' ||
+    req.body.gResp === undefined ||
+    req.body.gResp === null
+  ) {
+    res.status(400);
+    res.send({ error: 'Select google recaptcha' });
+    return;
+  }
+
+  const recaptchaResponse = await verifyRecaptcha(req.body.gResp);
+
+  if (!recaptchaResponse.success || recaptchaResponse.success === 'false') {
+    res.status(400);
+    res.send({ error: 'Invalid recaptcha' });
+    return;
+  }
+
   const id = await generateFiles(req.body);
   res.status(200);
   res.send({ id });
